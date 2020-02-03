7ab6d1f9660a6f665073c35bf71fa8e447c9643d
arek.pazola1998@gmail.com <arek.pazola1998@gmail.com>
2020-02-03 16:16:28 +0100

    Minor changes on Roll

--- BukBot/Modules/Roll.cs
+++ BukBot/Modules/Roll.cs
@@ -12,19 +12,27 @@ namespace BukBot.Modules
         [Command("Roll")]
         public async Task RollPatryk(params string[] rollArgs)
         {
-            if (rollArgs.Length != 3) await ReplyAsync("Dzban, @BukBot Roll {osoba} {kanał z} {kanał do}");
+            if (rollArgs.Length != 2)
+            {
+                await ReplyAsync("Dzban, @BukBot Roll {osoba} {kanał do}");
+                return;
+            }
             var userFromMessage = rollArgs[0];
-            var firstChannelFromMessage = rollArgs[1];
-            var secondChannelFromMessage = rollArgs[2];
+            var desitinationChannelFromMessage = rollArgs[1];
             var user = Context.Guild.Users.Where(u => u.Username == userFromMessage).FirstOrDefault();
-            var firstChannel = Context.Guild.VoiceChannels.Where(c => c.Name == firstChannelFromMessage).FirstOrDefault();
-            var secondChannel = Context.Guild.VoiceChannels.Where(c => c.Name == secondChannelFromMessage).FirstOrDefault();
-            await user.ModifyAsync(x => x.Channel = secondChannel);
-            await user.ModifyAsync(x => x.Channel = firstChannel);
-            await user.ModifyAsync(x => x.Channel = secondChannel);
-            await user.ModifyAsync(x => x.Channel = firstChannel);
-            await user.ModifyAsync(x => x.Channel = secondChannel);
-            await user.ModifyAsync(x => x.Channel = firstChannel);
+            var originChannel = user.VoiceChannel;
+            var destinationChannel = Context.Guild.VoiceChannels.Where(c => c.Name == desitinationChannelFromMessage).FirstOrDefault();
+            if (originChannel.Name == destinationChannel.Name)
+            {
+                await ReplyAsync("Dzban, nie możesz podać takiej samej nazwy kanału");
+                return;
+            }
+            await user.ModifyAsync(x => x.Channel = destinationChannel);
+            await user.ModifyAsync(x => x.Channel = originChannel);
+            await user.ModifyAsync(x => x.Channel = destinationChannel);
+            await user.ModifyAsync(x => x.Channel = originChannel);
+            await user.ModifyAsync(x => x.Channel = destinationChannel);
+            await user.ModifyAsync(x => x.Channel = originChannel);
             var users = Context.Guild.Users.Select(u => u.Username);
             await ReplyAsync($"Bokiem jest {user}");
         }
