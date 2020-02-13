90156861195b8145d09b52ec092f2e4450c257db
arek.pazola1998@gmail.com <arek.pazola1998@gmail.com>
2020-02-13 16:10:39 +0100

    Add Meme module with add meme command

---
+++ BukBot/Modules/Meme.cs
@@ -0,0 +1,39 @@
+﻿using Discord.Addons.Interactive;
+using Discord.Commands;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BukBot.Modules
+{
+    [Group("Meme")]
+    public class Meme : InteractiveBase<SocketCommandContext>
+    {
+        [Command("")]
+        [Summary("")]
+        [Remarks("")]
+        public async Task ShowListOfMemes()
+        {
+
+        }
+
+        [Command("Add")]
+        [Summary("")]
+        [Remarks("")]
+        public async Task AddMeme(string name)
+        {
+            using (var client = new HttpClient())
+            {
+                var file = await client.GetStringAsync(Context.Message.Attachments.First().Url);
+                var stream = new WebClient().OpenRead(Context.Message.Attachments.First().Url);
+
+                var img = Image.FromStream(stream, false, false);
+                img.Save(@$"C:\users\apaz02\desktop\{name}.png", ImageFormat.Png);
+                await ReplyAsync(await client.GetStringAsync(Context.Message.Attachments.First().Url));
+            }
+        }
+    }
+}
